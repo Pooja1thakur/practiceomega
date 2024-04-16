5a8fb1d88f4b2c6248aa9ef62de1c1e50e73f16a
Pooja1thakur <pooja2810rohit@gmail.com>
2024-04-17 00:23:40 +0530

    dom done

--- Javascripts/array.js
+++ Javascripts/array.js
@@ -8,3 +8,20 @@
 // ];
 // console.log(arr[1][3]);
 
+// let arr1=[1,2,3,4];
+// let arr2=[5,6,7,8];
+
+// let arr3=[...arr1,...arr2];
+// console.log(arr3);
+
+
+
+var arr=[3,4,5,6,7];
+var newarr= arr.map(function(x,i){
+console.log(i);
+return x*x;
+})
+console.log(newarr);
+
+
+
---
+++ Javascripts/date.js
@@ -0,0 +1,6 @@
+// var a=new Date();
+// console.log(a);
+
+
+var a=new Date().toLocaleString()
+console.log(a);
